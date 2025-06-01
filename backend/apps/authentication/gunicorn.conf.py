9ed47948e549645f6f3c1a1d64e27a8a5c590e00
Sam Nazarov <samnnazarov@gmail.com>
2025-06-02 01:14:25 +0300

    Change IP to 0.0.0.0 for gunicorn into docker

--- backend/apps/authentication/gunicorn.conf.py
+++ backend/apps/authentication/gunicorn.conf.py
@@ -6,7 +6,7 @@ def max_workers():
     return cpu_count()
 
 
-bind = '127.0.0.1:' + environ.get('PORT', '8000')
+bind = '0.0.0.0:' + environ.get('PORT', '8000')
 max_requests = 1000
 worker_class = 'gevent'
 workers = max_workers()
