038f4620cc9eff53a1919a9a1e2aab969fd4ac29
Sam Nazarov <samnnazarov@gmail.com>
2025-07-15 17:53:23 +0300

    Create custom User model

--- backend/services/auth/apps/authentication/models.py
+++ backend/services/auth/apps/authentication/models.py
@@ -1,3 +1,32 @@
-from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import MultipleObjectsReturned, ObjectDoesNotExist
 
-# Create your models here.
+
+class User(AbstractUser):
+    """
+    Users within the Django authentication system are represented by this
+    model.
+
+    Username and password are required. Other fields are optional.
+    """
+
+    class Meta(AbstractUser.Meta):
+        db_table = 'auth_user'
+
+    @classmethod
+    def get_user(cls, name):
+        try:
+            return cls.objects.get(username=name)
+        except MultipleObjectsReturned as e:
+            raise e
+        except ObjectDoesNotExist:
+            return
+
+    @classmethod
+    async def aget_user(cls, name):
+        try:
+            return await cls.objects.aget(username=name)
+        except MultipleObjectsReturned as e:
+            raise e
+        except ObjectDoesNotExist:
+            return
