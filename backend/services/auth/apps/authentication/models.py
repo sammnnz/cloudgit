67dec1201081a72ed74317d80ad8538b5015a572
Sam Nazarov <samnnazarov@gmail.com>
2025-07-28 06:19:48 +0300

    Update custom `User` model

--- backend/services/auth/apps/authentication/models.py
+++ backend/services/auth/apps/authentication/models.py
@@ -1,5 +1,25 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager as _UserManager
 from django.core.exceptions import MultipleObjectsReturned, ObjectDoesNotExist
+from django.db.models import EmailField
+from django.utils.translation import gettext_lazy as _
+
+
+class UserManager(_UserManager):
+    def get_safe(self, **kwargs):
+        try:
+            return self.model.objects.get(**kwargs)
+        except MultipleObjectsReturned:
+            raise
+        except ObjectDoesNotExist:
+            return None
+
+    async def aget_safe(self, **kwargs):
+        try:
+            return await self.model.objects.aget(**kwargs)
+        except MultipleObjectsReturned:
+            raise
+        except ObjectDoesNotExist:
+            return None
 
 
 class User(AbstractUser):
@@ -9,24 +29,17 @@ class User(AbstractUser):
 
     Username and password are required. Other fields are optional.
     """
+    email = EmailField(
+        _("email address"),
+        blank=True,
+        unique=True,
+        # validators=[validate_email],
+        # error_messages={
+        #     "unique": _("A user with that email already exists."),
+        # }
+    )
+
+    objects = UserManager()
 
     class Meta(AbstractUser.Meta):
         db_table = 'auth_user'
-
-    @classmethod
-    def get_user(cls, name):
-        try:
-            return cls.objects.get(username=name)
-        except MultipleObjectsReturned as e:
-            raise e
-        except ObjectDoesNotExist:
-            return
-
-    @classmethod
-    async def aget_user(cls, name):
-        try:
-            return await cls.objects.aget(username=name)
-        except MultipleObjectsReturned as e:
-            raise e
-        except ObjectDoesNotExist:
-            return
