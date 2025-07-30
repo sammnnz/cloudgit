b4c771d60c6098649a78dbda6cc610d7ec7f68ad
Sam Nazarov <samnnazarov@gmail.com>
2025-07-30 05:01:30 +0300

    Fix docker-compose and nginx config

--- up.sh
+++ up.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 docker build -t cloudgit-auth --build-context common=./backend/common  backend/services/auth
+docker build -t cloudgit-repo --build-context common=./backend/common  backend/services/repo
 docker compose -f docker-compose.yml up nginx -d
