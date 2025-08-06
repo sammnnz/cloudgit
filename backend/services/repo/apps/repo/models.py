5ad3bcc5e21aa50836e0d787eac9a870a986bc8e
Sam Nazarov <samnnazarov@gmail.com>
2025-08-07 02:43:23 +0300

    Update models and create migrations in `repo` service

--- backend/services/repo/apps/repo/models.py
+++ backend/services/repo/apps/repo/models.py
@@ -7,7 +7,8 @@ from django.db import models, IntegrityError
 from django.utils.translation import gettext_lazy as _
 # https://pypi.org/project/django-enum/
 from django_enum import EnumField
-from .managers import AuthUserExternalManager, StorageManager
+from .managers import (AuthUserExternalManager, MaitainerAccessEnum, RepoAccessEnum, RepoManager, StorageManager,
+                       StorageTypeEnum)
 
 LOGGER = logging.getLogger('repo')
 
@@ -40,7 +41,7 @@ async def _on_change_user(msg: AbstractIncomingMessage):
         raise CancelAcknowledge
 
     try:
-        await fn(**{"_id": _id, "username": username})
+        await fn(**{"user_id": _id, "username": username})
     except IntegrityError:
         LOGGER.warning(f"Failed on {action} of user '{username}', "
                        f"but incoming message is acknowledged.")
@@ -83,39 +84,79 @@ class File(models.Model):
 
 
 class Maintainer(models.Model):
-    class AccessEnum(models.TextChoices):
-        FULL = 'f', 'full'
-        PARTIAL = 'p', 'partial'
-
     id = models.BigAutoField(primary_key=True)
     repo = models.ForeignKey('Repo', on_delete=models.CASCADE)
     user = models.ForeignKey(AuthUserExternal, on_delete=models.CASCADE, db_index=False)
-    access = EnumField(AccessEnum, db_comment='values: full, partial')
+    access = EnumField(MaitainerAccessEnum, db_comment='values: full, partial')
 
     class Meta:
         db_table = 'maintainer'
 
 
 class Repo(models.Model):
-    class AccessEnum(models.TextChoices):
-        PRIVATE = 'PR', 'private'
-        PUBLIC = 'PU', 'public'
-
     id = models.BigAutoField(primary_key=True)
     user = models.ForeignKey(AuthUserExternal, on_delete=models.CASCADE, db_comment='repo holder')
-    storage = models.ForeignKey('Storage', on_delete=models.SET_NULL, blank=True, null=True,
-                                db_comment='`NULL` when repo store locale')
-    name = models.CharField(max_length=32)
-    access = EnumField(AccessEnum, db_index=True, db_comment='values: private, public')
-    path = models.CharField(max_length=255, db_comment='path into storage')
+    storage = models.ForeignKey('Storage', on_delete=models.SET_NULL, blank=False, null=True,
+                                db_comment='local or remote')
+    repo_name = models.CharField(
+        blank=False,
+        max_length=32,
+        error_messages={
+            'max_length': _("Repo name must be less than 32 characters."),
+        }
+    )
+    access = EnumField(RepoAccessEnum, db_index=True, db_comment='values: private, public')
+    description = models.CharField(_("repo description"), blank=False, max_length=50)
+    path = models.CharField(max_length=255, blank=False, db_comment='path into storage')
+
+    objects = RepoManager()
 
     class Meta:
         db_table = 'repo'
+        unique_together = ('user', 'repo_name')
 
 
 class Storage(models.Model):
     id = models.BigAutoField(primary_key=True)
-    link = models.CharField(max_length=255, blank=False)
+    name = models.CharField(
+        max_length=32,
+        unique=True,
+        blank=False,
+        help_text=_(
+            "Required. May be unique."
+        ),
+        error_messages={
+            'max_length': _("Storage name must be less than 32 characters."),
+        }
+    )
+    link = models.CharField(
+        _("storage link"),
+        max_length=255,
+        blank=False,
+        null=True,
+        help_text=_(
+            "Required. Link to remote storage. If storage is local, then set null."
+        ),
+        error_messages={
+            'max_length': _("Storage link must be less than 255 characters."),
+        }
+    )
+    type = EnumField(
+        StorageTypeEnum,
+        verbose_name=_("storage type"),
+        blank=False,
+        db_comment='values: local, remote',
+        help_text=_("Required. Type of repo may be 'local' or 'remote'.'")
+    )
+    path = models.CharField(
+        _("storage path"),
+        max_length=255,
+        blank=False,
+        help_text=_("Required. Path into storage server."),
+        error_messages={
+            'max_length': _("Path must be less than 255 characters."),
+        }
+    )
 
     objects = StorageManager()
 
